baf99b9683403a0dd7b8b570bd351e3bcc5f53e4
P Marecki <pmarecki.pro@gmail.com>
2018-10-27 09:57:50 +0200

    (opcjonalne) monitorowanie

--- src/main/java/wsi/fin/FinControler.java
+++ src/main/java/wsi/fin/FinControler.java
@@ -1,7 +1,10 @@
 package wsi.fin;
 
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +16,26 @@ import wsi.model.Todo;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @CrossOrigin
 @Slf4j
-public class FinControler {
+public class FinControler implements InitializingBean {
+
+    //do metryk/monitorwania
+    @Autowired
+    MeterRegistry registry;
+
+    AtomicInteger kurs;
+
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        kurs = registry.gauge("kurs_USD", new AtomicInteger(0));
+    }
 
     @RequestMapping(value = "/fin_status", method = RequestMethod.GET)
     public String getStatus() {
@@ -28,8 +46,10 @@ public class FinControler {
     //https://www.baeldung.com/spring-scheduled-tasks
 
 
-    @Scheduled(cron = "0/10 * * * * ?")
+    @Scheduled(cron = "0/5 * * * * ?")
     public void doSomethingInSchduledWay() {
+        Random r = new Random();
+        kurs.set(r.nextInt(100) + 10);
         log.info("doing...");
     }
 
