beb9a68c9420e9e5077e747c6371fa088c1774ad
P Marecki <pmarecki.pro@gmail.com>
2018-11-17 07:34:50 +0100

    Add command execution

---
+++ src/main/java/wsi/exec/TryIt.java
@@ -0,0 +1,34 @@
+package wsi.exec;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TryIt {
+    public static void main(String[] args) {
+        try {
+            String s = "";
+            Process p = Runtime.getRuntime().exec("ps -ef");
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
