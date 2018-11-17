8a4beaf88e8dc79eac4302996f535b78e3f2d264
P Marecki <pmarecki.pro@gmail.com>
2018-11-17 08:39:03 +0100

    working basic version

---
+++ src/main/java/wsi/exec/MainController.java
@@ -0,0 +1,104 @@
+package wsi.exec;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@RestController
+@CrossOrigin
+@Slf4j
+public class MainController {
+
+//    @RequestMapping(value = "/users/create", method = GET)
+//    public User createUser(
+//            @RequestParam(value = "nick") String nick) {
+//        log.info("Attempt to create user with nick [{}]", nick);
+//        return userService.createUser(nick);
+//    }
+
+    @RequestMapping(value = "/status", method = GET)
+    public String getStatus() {
+        return "OK";
+    }
+
+//    @RequestMapping(value = "/users",
+//            method = GET)
+//    public List<User> getUsers() {
+//        log.info("Attempt to get all " +
+//                "user names");
+//        return userService.getUsers();
+//    }
+//
+//
+//
+//    ////////////////////////////////////////////
+//    //ROOMS
+//
+//
+//    @RequestMapping(value = "/rooms/create", method = GET)
+//    public Room createRoom(
+//            @RequestParam(value = "roomid") String roomid) {
+//        log.info("Attempt to create room with id[{}]", roomid);
+//        return roomService.createRoom(roomid);
+//    }
+//
+//    @RequestMapping(value = "/rooms",
+//            method = GET)
+//    public List<Room> getRooms() {
+//        log.info("Attempt to get all " +
+//                "room names");
+//        return roomService.getRooms();
+//    }
+//
+//
+//
+//    ////////////////////////////////////////////
+//
+//    //MESSAGES
+//
+//    //CRUD (create read update delete)
+//
+//    @RequestMapping(value = "/rooms/messages", method = GET)
+//    public List<Message> getAllMessagesInRoom(
+//            @RequestParam(value = "roomid") String roomid) {
+//        return messageService.getByRoom(roomid);
+//    }
+//
+//    @RequestMapping(value = "/rooms/messages", method = POST)
+//    public Message postMessage(@RequestBody Message newMessage) {
+//        return messageService.create(newMessage);
+//    }
+//
+//    @RequestMapping(value = "/rooms/messages", method = PUT)
+//    public Message updateMessage(@RequestBody Message toUpdate) {
+//        return messageService.update(toUpdate);
+//    }
+//
+//    @RequestMapping(value = "/rooms/messages", method = DELETE)
+//    public Response deleteMessage(
+//            @RequestParam(value = "messageId") Integer messageId) {
+//        messageService.delete(messageId);
+//        return new Response("OK");
+//    }
+//
+//    /// VOTING
+////    @RequestMapping(value = "/rooms/messages/upvote", method = GET)
+////    public Room upvoteMessage(@RequestParam(value = "messageid") String messageid) {
+////        return roomService.createRoom(roomid);
+////    }
+////    @RequestMapping(value = "/rooms/messages/downvote", method = GET)
+////    public Room downvoteMessage(@RequestParam(value = "messageid") String messageid) {
+////        return roomService.createRoom(roomid);
+////    }
+////    @RequestMapping(value = "/rooms/messages/flag", method = GET)
+////    public Room flagMessage(@RequestParam(value = "messageid") String messageid) {
+////        return roomService.createRoom(roomid);
+////    }
+//
+//
+
+
+}
